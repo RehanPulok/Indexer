2afba04e95e0805ae094b92cf38d1636820e9fc5
18-37907-2@student.aiub.edu <18-37907-2@student.aiub.edu>
2021-03-22 09:13:18 +0600

    Add project files.

---
+++ Indexer/Indexer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class Indexer
+    {
+
+        private string[] cities = new string[5];
+
+
+        public string this[int index]
+        {
+            get
+            {
+
+                return cities[index];
+            }
+
+            set
+            {
+                cities[index] = value;
+            }
+        }
+    }
+}
+
+   
---
+++ Indexer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Indexer index = new Indexer();
+            index[0] = "DHAKA";
+            index[1] ="CHITTAGONG";
+            index[2] ="BARISHAL";
+            index[3] ="RAJSHAHI";
+            index[4] ="KHULNA";
+            Console.WriteLine("Write a name of the city: ");
+            string city = Console.ReadLine().ToUpper();
+            int found = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                if (city == index[i])
+                {
+                    Console.WriteLine("City found");
+                    found = found + 1;
+                    break;
+                }
+                
+            }
+            if (found==0)
+            {
+                Console.WriteLine("City not found");
+            }
+            
+            Console.ReadLine();
+            
+
+        }
+    }
+}
